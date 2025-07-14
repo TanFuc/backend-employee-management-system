cb898f3f43e0b21bd2667149fa93997b42df432e
TanFuc <nguyentannphuc@gmail.com>
2025-07-14 14:34:20 +0700

    add core entities 2

--- src/main/java/com/java/backend/entity/RolePermissionEntity.java
+++ src/main/java/com/java/backend/entity/RolePermissionEntity.java
@@ -1,10 +1,10 @@
 package com.java.backend.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,13 +12,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "permissions")
-public class PermissionEntity {
+@Table(name = "role_permission")
+public class RolePermissionEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String name;
+    @ManyToOne
+    private RoleEntity role;
+
+    @ManyToOne
+    private PermissionEntity permission;
 }
 
